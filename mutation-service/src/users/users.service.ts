a275b9e449020f9209b59d6d7b84f6828a0fc8df
Gabriel Mendieta <gamendietap@gmail.com>
2025-04-23 15:54:59 -0400

    Hash metodo Update

--- mutation-service/src/users/users.service.ts
+++ mutation-service/src/users/users.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { Prisma } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
     constructor(private readonly prisma: PrismaService) { }
 
-    async create(data: Prisma.UserCreateInput) {
+    async create(data: CreateUserDto) {
         const hashedPassword = await bcrypt.hash(data.password, 10);
         return this.prisma.user.create({
             data: {
@@ -18,10 +20,15 @@ export class UsersService {
         });
     }
 
-    async update(id: string, data: Prisma.UserUpdateInput) {
+    async update(id: string, data: UpdateUserDto) {
+        const hashedPassword = await bcrypt.hash(data.password!, 10);
         return this.prisma.user.update({
             where: { id },
-            data,
+            data: {
+                name: data.name,
+                email: data.email,
+                password: hashedPassword,
+            },
         });
     }
 
